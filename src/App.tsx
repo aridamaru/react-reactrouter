808ad04b890b66418f26deea8675e771a789d393
aridamaru7@gmail.com <aridamaru7@gmail.com>
2020-04-15 02:21:48 +0400

    Create List item add functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
 import React, { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
 
-const App: React.FunctionComponent = () => {
+declare var confirm: (question: string) => boolean;
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    };
+    //setTodos([newTodo, ...todos]);
+    setTodos(prev => [newTodo, ...prev]);
+  };
+
+  const toggleHandler = (id: number) => {
+    setTodos(prev =>
+      prev.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    const shouldRemove = confirm("Are you sure?");
+    if (shouldRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id));
+    }
+  };
+
   return (
     <>
       <Navbar />
       <div className="container">
-        <TodoForm />
+        <TodoForm onAdd={addHandler} />
+
+        <TodoList
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+        />
       </div>
     </>
   );
